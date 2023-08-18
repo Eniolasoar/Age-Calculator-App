fd2f893270b7a780f950b7f58acd2f1abf9a62b2
Eniolasoar <eniolanishe@gmail.com>
2023-08-18 22:03:01 +0100

    website skeleton restructuring

--- validation.js
+++ validation.js
@@ -1,165 +1,213 @@
-document.querySelector("#button").addEventListener('click',function(){
+document.querySelector("#button").addEventListener("click", function () {
+  let dayInput = document.getElementById("day");
+  let monthInput = document.getElementById("month");
+  let yearInput = document.getElementById("year");
 
-    let button=document.getElementById("button");
-    button.style.color="hsl(259, 100%, 65%)";
-    //input
-    let dayInput=document.getElementById("day");
-    let monthInput=document.getElementById("month");
-    let yearInput=document.getElementById("year");
+  //input value
+  let day = document.querySelector("#day").value;
+  let month = document.querySelector("#month").value;
+  let year = document.querySelector("#year").value;
 
-    //input value
-    let day=document.querySelector("#day").value;
-    let  month=document.querySelector("#month").value;
-    let year=document.querySelector("#year").value;
+  // day=parseInt(day);
+  // month=parseInt(month);
 
-    //input labels
-    let dayLabel=document.getElementById("dayLabel");
-    let monthLabel=document.getElementById("monthLabel");
-    let yearLabel =document.getElementById("yearLabel");
+  //input labels
+  let dayLabel = document.getElementById("dayLabel");
+  let monthLabel = document.getElementById("monthLabel");
+  let yearLabel = document.getElementById("yearLabel");
 
-    //input error message
-    let errorMessage1=document.getElementById("errorMessage1");
-    let errorMessage2=document.getElementById("errorMessage2");
-    let errorMessage3=document.getElementById("errorMessage3")
+  //input error message
+  let errorMessage1 = document.getElementById("errorMessage1");
+  let errorMessage2 = document.getElementById("errorMessage2");
+  let errorMessage3 = document.getElementById("errorMessage3");
 
-    if(day==""){
-        dayInput.style.border="1px solid red";
-        dayLabel.style.color="red";
-        errorMessage1.innerHTML="This field is required";
-        errorMessage1.style.color="red";
-    }
-    if(month =="" ){
-        monthInput.style.border="1px solid red";
-        monthLabel.style.color="red";
-        errorMessage2.innerHTML="This field is required";
-        errorMessage2.style.color="red";
-    }
-    if(year ==""){
-        yearInput.style.border="1px solid red";
-        yearLabel.style.color="red"
-        errorMessage3.innerHTML="This field is required";
-        errorMessage3.style.color="red";
-    }
-    if(day<1 || day>31){
-        dayInput.style.border="1px solid red";
-        dayLabel.style.color="red";
-        errorMessage1.innerHTML="Please enter a valid date!";
-        errorMessage1.style.color="red";
+  if (day == "") {
+    dayInput.style.border = "1px solid red";
+    dayLabel.style.color = "red";
+    errorMessage1.innerHTML = "This field is required";
+    errorMessage1.style.color = "red";
+  }
+  if (month == "") {
+    monthInput.style.border = "1px solid red";
+    monthLabel.style.color = "red";
+    errorMessage2.innerHTML = "This field is required";
+    errorMessage2.style.color = "red";
+  }
+  if (year == "") {
+    yearInput.style.border = "1px solid red";
+    yearLabel.style.color = "red";
+    errorMessage3.innerHTML = "Required field";
+    errorMessage3.style.color = "red";
+  }
+  if (day < 1 || day > 31) {
+    dayInput.style.border = "1px solid red";
+    dayLabel.style.color = "red";
+    errorMessage1.innerHTML = "Enter a valid day!";
+    errorMessage1.style.color = "red";
+  }
+  if (month < 1 || month > 12) {
+    monthInput.style.border = "1px solid red";
+    monthLabel.style.color = "red";
+    errorMessage2.innerHTML = "Invalid month";
+    errorMessage2.style.color = "red";
+  }
+  let currentYear = new Date().getFullYear();
+  if (year > currentYear) {
+    yearInput.style.border = "1px solid red";
+    yearLabel.style.color = "red";
+    errorMessage3.innerHTML = "Please enter a valid year";
+    errorMessage3.style.color = "red";
+  }
 
-    }
-    if(month<1 || month>12){
-        monthInput.style.border="1px solid red";
-        monthLabel.style.color="red";
-        errorMessage2.innerHTML="Please enter a valid month";
-        errorMessage2.style.color="red";
-    }
-    let currentYear=new Date().getFullYear();
-    if(year>currentYear){
-        yearInput.style.border="1px solid red";
-        yearLabel.style.color="red";
-        errorMessage3.innerHTML="Please enter a valid year";
-        errorMessage3.style.color="red";
-    }
+  if (checkValidDate(day, month) == "false") {
+    dayInput.style.border = "1px solid red";
+    dayLabel.style.color = "red";
+    errorMessage1.innerHTML = "Please enter a valid date!";
+    errorMessage1.style.color = "red";
+  }
 
-    if(day!=="" && month!=="" && year!=="" ){
-        if(day>=1 || day<=31){
-            if(month>=1 || month<=12){
-                if(year>currentYear){
-                    calculateAge();
-                }
-            }
+  if (day !== "" && month !== "" && year !== "") {
+    if (day >= 1 || day <= 31) {
+      if (month >= 1 || month <= 12) {
+        if (year <= currentYear) {
+          if (checkValidDate(day, month) == "true") {
+            console.log(day);
+            console.log(month);
+            console.log(checkValidDate(day, month));
+            calculateAge();
+          }
         }
-        
+      }
     }
+  }
 
-    dayInput.addEventListener('input',function(){
-        applyDayStyle(this,dayLabel,errorMessage1);
-    });
+  dayInput.addEventListener("input", function () {
+    applyDayStyle(this, dayLabel, errorMessage1);
+  });
 
-    monthInput.addEventListener('input',function(){
-        applyMonthStyle(this,monthLabel,errorMessage2);
-    });
+  monthInput.addEventListener("input", function () {
+    applyMonthStyle(this, monthLabel, errorMessage2);
+  });
 
-    yearInput.addEventListener('input',function(){
-        applyYearStyle(this,yearLabel,errorMessage3);
-    })
-
-
-    
+  yearInput.addEventListener("input", function () {
+    applyYearStyle(this, yearLabel, errorMessage3);
+  });
 });
 
-//creating seperating functions for each styling and validation 
-function applyDayStyle(element,elementLabel,elementOutput){
-    if (element.value == "") {
-        element.style.border = "1px solid red";
-        elementLabel.style.color="red";
-        elementOutput.innerHTML = "This field is required ";
-        elementOutput.style.color="red";
-      }  
-      if(element.value<1 | element.value>31){
-        element.style.border="1px solid red";
-        elementLabel.style.color="red";
-        elementOutput.innerHTML="Please enter a valid date!";
-        elementOutput.style.color="red";
+//creating seperating functions for each styling and validation
+function applyDayStyle(element, elementLabel, elementOutput) {
+  if (element.value == "") {
+    element.style.border = "1px solid red";
+    elementLabel.style.color = "red";
+    elementOutput.innerHTML = "This field is required ";
+    elementOutput.style.color = "red";
+  }
+  if ((element.value < 1) | (element.value > 31)) {
+    element.style.border = "1px solid red";
+    elementLabel.style.color = "red";
+    elementOutput.innerHTML = "Please enter a valid date!";
+    elementOutput.style.color = "red";
+  }
+  let monthDate = document.querySelector("#month").value;
+  if (checkValidDate(element.value, monthDate) == false) {
+    dayInput.style.border = "1px solid red";
+    dayLabel.style.color = "red";
+    errorMessage1.innerHTML = "Please enter a valid date for this month!";
+    errorMessage1.style.color = "red";
+  }
 
-    }
-    if(element.value!=="" &&((element.value>=1 && element.value<=31))){
-        
-                    element.style.border = "1px solid green";
-                    elementLabel.style.color="grey";
-                    elementOutput.innerHTML = "";
-    }
+  if (
+    element.value !== "" &&
+    element.value >= 1 &&
+    element.value <= 31 &&
+    checkValidDate(element.value, monthDate)
+  ) {
+    element.style.border = "1px solid green";
+    elementLabel.style.color = "grey";
+    elementOutput.innerHTML = "";
+  }
 }
 
-function applyMonthStyle(element,elementLabel,elementOutput){
-    if (element.value == "") {
-        element.style.border = "1px solid red";
-        elementLabel.style.color="red";
-        elementOutput.innerHTML = "This field is required ";
-        elementOutput.style.color="red";
-      }  
-      if(element.value<1 || element.value>12){
-        element.style.border="1px solid red";
-        elementLabel.style.color="red";
-        elementOutput.innerHTML="Please enter a valid date!";
-        elementOutput.style.color="red";
-
+function applyMonthStyle(element, elementLabel, elementOutput) {
+  if (element.value == "") {
+    element.style.border = "1px solid red";
+    elementLabel.style.color = "red";
+    elementOutput.innerHTML = "This field is required ";
+    elementOutput.style.color = "red";
+  }
+  if (element.value < 1 || element.value > 12) {
+    element.style.border = "1px solid red";
+    elementLabel.style.color = "red";
+    elementOutput.innerHTML = "Please enter a valid date!";
+    elementOutput.style.color = "red";
+  }
+  if (element.value !== "") {
+    if (element.value >= 1 && element.value <= 12) {
+      element.style.border = "1px solid green";
+      elementLabel.style.color = "grey";
+      elementOutput.innerHTML = "";
     }
-    if(element.value!==""){
-        if(element.value>=1 && element.value<=12){
-            element.style.border = "1px solid green";
-                    elementLabel.style.color="grey";
-                    elementOutput.innerHTML = "";
-        }
-    }
+  }
 }
 
-function applyYearStyle(element,elementLabel,elementOutput){
-    if (element.value == "") {
-        element.style.border = "1px solid red";
-        elementLabel.style.color="red";
-        elementOutput.innerHTML = "This field is required ";
-        elementOutput.style.color="red";
-      }  
-      let currentYear=new Date().getFullYear();
-    if(element.value>currentYear){
-        element.style.border="1px solid red";
-        elementLabel.style.color="red";
-        elementOutput.innerHTML="Please enter a valid date!";
-        elementOutput.style.color="red";
-
+function applyYearStyle(element, elementLabel, elementOutput) {
+  if (element.value == "") {
+    element.style.border = "1px solid red";
+    elementLabel.style.color = "red";
+    elementOutput.innerHTML = "This field is required ";
+    elementOutput.style.color = "red";
+  }
+  let currentYear = new Date().getFullYear();
+  if (element.value > currentYear) {
+    element.style.border = "1px solid red";
+    elementLabel.style.color = "red";
+    elementOutput.innerHTML = "Please enter a valid date!";
+    elementOutput.style.color = "red";
+  }
+  if (element.value !== "") {
+    if (element.value < currentYear) {
+      element.style.border = "1px solid green";
+      elementLabel.style.color = "grey";
+      elementOutput.innerHTML = "";
     }
-    if(element.value!==""){
-        if(element.value<currentYear){
-            element.style.border = "1px solid green";
-                    elementLabel.style.color="grey";
-                    elementOutput.innerHTML = "";
-        }
-    }
+  }
 }
 
-function calculateAge(){
-    let currentDate = new Date();
+//a function to check the validity of a date(emphasis on the day)
+function checkValidDate(day, month) {
+  let valid = "true";
+  //using switch statement
+  switch (month) {
+    case 1:
+    case 3:
+    case 5:
+    case 7:
+    case 8:
+    case 10:
+    case 12:
+      if (day > 31) {
+        valid = "false";
+      }
+      break;
+    case 2:
+      //February day is assumed to be 28 days
+      if (day > 28) {
+        valid = "false";
+      }
+      break;
+    case 4:
+    case 6:
+    case 9:
+    case 11:
+      if (day > 30) {
+        valid = "false";
+      }
+      break;
+  }
+  return valid;
+}
+function calculateAge() {
+  let currentDate = new Date();
 
   //This is the output location
   let dayOutput = document.querySelector(".dayOutput");
@@ -181,7 +229,7 @@ function calculateAge(){
 
   // Check if the birthday hasn't occurred yet this year
   //if current month is lesser than birth month
-  if (month > monthDate|| (month === monthDate && day > dayDate)) {
+  if (month > monthDate || (month === monthDate && day > dayDate)) {
     calculatedYear -= 1;
   }
 
@@ -192,8 +240,6 @@ function calculateAge(){
     const daysInPrevMonth = (currentDate - prevMonthDate) / (1000 * 3600 * 24);
     calculatedDay += daysInPrevMonth;
   }
-
-
 
   yearOutput.innerHTML = calculatedYear;
   monthOutput.innerHTML = calculatedMonth;
